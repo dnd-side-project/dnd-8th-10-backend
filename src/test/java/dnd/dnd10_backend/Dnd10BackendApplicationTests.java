04039c82bfb16f1a8c43f6ac026d38f8292cb4e0
sudhdkso <waga1009@gmail.com>
2023-01-16 23:18:12 +0900

    set:기본설정

---
+++ src/test/java/dnd/dnd10_backend/Dnd10BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dnd.dnd10_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dnd10BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
