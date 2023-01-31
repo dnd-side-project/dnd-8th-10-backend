85ccf5a762534d0c7e41733b5729be02b17e883a
WonJ1yun <78727847+sudhdkso@users.noreply.github.com>
2023-01-31 20:50:50 +0900

    Revert "set: db 설정"

--- src/test/java/dnd/dnd10_backend/Dnd10BackendApplicationTests.java
+++ src/test/java/dnd/dnd10_backend/Dnd10BackendApplicationTests.java
@@ -3,7 +3,7 @@ package dnd.dnd10_backend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-//@SpringBootTest
+@SpringBootTest
 class Dnd10BackendApplicationTests {
 
     @Test
