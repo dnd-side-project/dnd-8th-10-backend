59356ae08e9870921d958242e41b83039fb34c6f
sudhdkso <waga1009@gmail.com>
2023-02-26 02:38:03 +0900

    fix: 사용자 wage 필드 누락 된 거 추가

--- src/main/java/dnd/dnd10_backend/user/domain/User.java
+++ src/main/java/dnd/dnd10_backend/user/domain/User.java
@@ -1,5 +1,6 @@
 package dnd.dnd10_backend.user.domain;
 
+import dnd.dnd10_backend.auth.domain.Token;
 import dnd.dnd10_backend.calendar.domain.TimeCard;
 import dnd.dnd10_backend.checkList.domain.CheckList;
 import dnd.dnd10_backend.store.domain.Store;
@@ -37,6 +38,7 @@ import java.util.List;
  * [2023-02-11] workPlace 삭제
  * [2023-02-20] 체크리스트에 대한 연관 관계 추가 - 원지윤
  * [2023-02-20] timecard 연관 관계 삭제 - 이우진
+ * [2023-02-24] 토큰 연관관계 추가 - 원지윤
  */
 @Entity
 @Data
@@ -70,6 +72,9 @@ public class User implements UserDetails {
     @Column(name = "work_time")
     private String workTime;
 
+    @Column(name = "wage")
+    private double wage;
+
     @Column(name = "user_role")
     private String userRole;
 
@@ -78,20 +83,25 @@ public class User implements UserDetails {
     private Timestamp createTime;
 
     @ManyToOne
-    @JoinColumn(name = "store_idx", insertable = false, updatable = false)
+    @JoinColumn(name = "store_idx", insertable = false)
     private Store store;
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.REMOVE)
     private List<CheckList> CheckList = new ArrayList<>();
 
+    @OneToOne(mappedBy = "user", cascade = CascadeType.REMOVE)
+    @JoinColumn(name = "id")
+    private Token token;
+
     @Builder
     public User(Long kakaoId, String kakaoNickname,
-                String kakaoEmail, String userRole) {
+                String kakaoEmail, String userRole, double wage) {
 
         this.kakaoId = kakaoId;
         this.kakaoNickname = kakaoNickname;
         this.kakaoEmail = kakaoEmail;
         this.userRole = userRole;
+        this.wage = wage;
     }
 
     @Builder(builderMethodName = "dtoBuilder")
@@ -99,6 +109,7 @@ public class User implements UserDetails {
         this.role = requestDto.getRole();
         this.phoneNumber = requestDto.getPhoneNumber();
         this.workTime = requestDto.getWorkTime();
+        this.wage = requestDto.getWage();
         this.store = store;
     }
 
