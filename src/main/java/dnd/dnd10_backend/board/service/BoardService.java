83314ada019e7b22bed156dd3ef34199465e6026
woozxn <squib0612@gmail.com>
2023-03-01 23:22:52 +0900

    fix: 카테고리별 리스트 조회 로직 수정

--- src/main/java/dnd/dnd10_backend/board/service/BoardService.java
+++ src/main/java/dnd/dnd10_backend/board/service/BoardService.java
@@ -128,20 +128,20 @@ public class BoardService {
 
     public List<PostListResponseDto> getPostList(String category, User user) {
 
-        List<Post> posts = new ArrayList<>();
-
         if(category == "all") {
-            posts = postRepository.findByStore(user.getStore());
+            List<Post> posts = postRepository.findByStore(user.getStore());
+            List<PostListResponseDto> postList = posts.stream()
+                    .map(p -> new PostListResponseDto(p.getId(), p.getTitle(), p.getCategory(), p.getCheckCount(), p.getUserName(), p.getRole(), p.getCreateDate(), p.getModifiedDate()))
+                    .collect(Collectors.toList());
+
+            return postList;
         } else {
-            posts = postRepository.findByCategoryAndStore(category, user.getStore());
+            List<Post> posts = postRepository.findByCategoryAndStore(category, user.getStore());
+            List<PostListResponseDto> postList = posts.stream()
+                    .map(p -> new PostListResponseDto(p.getId(), p.getTitle(), p.getCategory(), p.getCheckCount(), p.getUserName(), p.getRole(), p.getCreateDate(), p.getModifiedDate()))
+                    .collect(Collectors.toList());
+
+            return postList;
         }
-
-
-        List<PostListResponseDto> postList = posts.stream()
-                .map(p -> new PostListResponseDto(p.getId(), p.getTitle(), p.getCategory(), p.getCheckCount(), p.getUserName(), p.getRole(), p.getCreateDate(), p.getModifiedDate()))
-                .collect(Collectors.toList());
-
-        return postList;
-
     }
 }
