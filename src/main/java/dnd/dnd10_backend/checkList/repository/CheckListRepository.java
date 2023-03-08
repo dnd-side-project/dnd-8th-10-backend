34063e35f2e0b5b23858140488fdbc5582e086ba
sudhdkso <waga1009@gmail.com>
2023-03-08 22:47:52 +0900

    fix: checkDate 이름 에러 해결

--- src/main/java/dnd/dnd10_backend/checkList/repository/CheckListRepository.java
+++ src/main/java/dnd/dnd10_backend/checkList/repository/CheckListRepository.java
@@ -27,12 +27,12 @@ import java.util.Optional;
 public interface CheckListRepository extends JpaRepository<CheckList, Long> {
     @Query(value = "select * from (select * from check_list c1 where c1.user_code = :user and c1.check_date = :date and c1.status = 'N' order by c1.check_idx DESC LIMIT 1000000) as A " + "UNION ALL "
             +"select * from ( select * from check_list c2 where c2.user_code = :user and c2.check_date = :date and c2.status = 'Y' order by c2.checked_time ASC LIMIT 1000000) as B", nativeQuery=true)
-    List<CheckList> findCheckListByDateAndUser(@Param("date")LocalDate date, @Param("user") User user);
+    List<CheckList> findCheckListByCheckDateAndUser(@Param("date")LocalDate date, @Param("user") User user);
 
     @Override
     Optional<CheckList> findById(Long checkIdx);
 
-    public List<CheckList> findCheckListByDateAndAndStatusAndUser(LocalDate date, String status, User user);
+    public List<CheckList> findCheckListByCheckDateAndAndStatusAndUser(LocalDate date, String status, User user);
 
     @Query("select c from CheckList c where c.checkDate <= :date")
     public List<CheckList> findCheckListByPastDate(@Param("date") LocalDate date);
