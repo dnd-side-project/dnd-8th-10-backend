8a826c1b273770f83df18fbd9ca3e8e4729390be
sudhdkso <waga1009@gmail.com>
2023-03-15 00:52:40 +0900

    chore: 불필요한 코드 삭제

--- src/main/java/dnd/dnd10_backend/Inventory/service/InventoryRecordService.java
+++ src/main/java/dnd/dnd10_backend/Inventory/service/InventoryRecordService.java
@@ -145,16 +145,7 @@ public class InventoryRecordService {
             }
             LocalDateTime endTime = LocalDateTime.parse(year +"-"+month+"-"+day+" "+HM2[0]+":"+HM2[1]+":00", formatter);
 
-            System.out.println("=======================");
-            System.out.println(pointTime.isBefore(startTime) );
-            System.out.println(pointTime.plusDays(1).isAfter(startTime));
-            System.out.println("=======================");
-            System.out.println(startTime.isBefore(pointTime));
-            System.out.println(startTime.isAfter(pointTime.plusDays(1)));
-            System.out.println("=======================");
-
-
-            if ((pointTime.isBefore(startTime) && pointTime.plusDays(1).isAfter(startTime))|| startTime.isEqual(now) || endTime.isEqual(now)) {
+            if ((pointTime.isBefore(startTime) && pointTime.plusDays(1).isAfter(startTime))|| startTime.isEqual(pointTime)) {
 
 
                 List<InventoryUpdateRecord> recordList = recordRepository.findByTimeCard(t);
